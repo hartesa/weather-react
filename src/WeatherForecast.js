25404e67a5a5f7c7ac701efd82bb5ed11a9cf14a
hartesa <106516372+hartesa@users.noreply.github.com>
2022-12-01 14:18:16 +0700

    import useEffect from react

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,24 +6,17 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [forecast, setForecast] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  // let [longitude, setLongitude] = useState(null);
-  // let [latitude, setLatitude] = useState(null);
 
-  // useEffect(() => {
-  //   setLoaded(false);
-  // }, [props.weatherData.coordinates]);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.weatherData.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
-    // setLongitude(response.data.lon);
-    // setLatitude(response.data.lat);
   }
 
   if (loaded) {
-    // console.log(forecast);
-    // console.log(props.weatherData);
-    // let iconCode = forecast[0].weather[0].icon;
     return (
       <div className="WeatherForecast flex-container">
         {forecast.map(function (dailyForecast, index) {
@@ -35,28 +28,9 @@ export default function WeatherForecast(props) {
             );
           }
         })}
-        {/* <div className="flex-item">
-          <WeatherForecastDay forecast={forecast[1]} />
-        </div>
-        <div className="flex-item">
-          <WeatherForecastDay forecast={forecast[2]} />
-        </div>
-        <div className="flex-item">
-          <WeatherForecastDay forecast={forecast[3]} />
-        </div>
-        <div className="flex-item">
-          <WeatherForecastDay forecast={forecast[4]} />
-        </div>
-        <div className="flex-item">
-          <WeatherForecastDay forecast={forecast[5]} />
-        </div>
-        <div className="flex-item">
-          <WeatherForecastDay forecast={forecast[6]} />
-        </div> */}
       </div>
     );
   } else {
-    console.log("masuk weatherforecast");
     // let apiKey = "866a208a73eeff02182218e9441647a1";
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.weatherData.coordinates.lon;
