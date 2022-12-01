a439a2923ff44dedb367b2c68f36c28b050fd693
hartesa <106516372+hartesa@users.noreply.github.com>
2022-12-01 13:34:15 +0700

    update eslint env
    node: true

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
